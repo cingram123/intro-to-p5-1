77a4b945257ddf56a3d060026fb7f61569263a37
Chase Ingram <cingram@wlapcs.org>
2017-03-01 04:17:41 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function steup(){
+    createCanvas(660,400);
+    background(12,102,247);
+}
+
+function draw(){
+    line(35,80,400,200);
+    
+    stroke(225,225,225);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,225);
+    ellipse(50,250,80,80);
+    
+    
+}
+
+// draw me 2 ellipses
+// draw me 2 rectangles
+// draw me 2 lines(1 with a positive slop, 1 with neg slop,)
+// 1 with a slope of 0, and 1 with a slope of undefined.
